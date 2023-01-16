74c517d894f914cec99eb22ad770a0f95ab4841b
Hasib74 <hasibakon74@gmail.com>
2023-01-16 20:27:54 +0600

    * Mason Cli

--- lib/core/home_screen.dart
+++ lib/core/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flavors_test/app/my_app.dart';
 import 'package:flavors_test/core/appConfig/app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:driver_hiring_app/main.dart';
+import 'package:driver_hiring_app/presentation/auth/signIn/sign_in.dart';
+import 'package:driver_hiring_app/core/appRoutes/app_routes.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -15,8 +18,12 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: InkWell(
             onTap: () {
-              Navigator.push(context,
-                  new MaterialPageRoute(builder: (_) => new MyAppTwo()));
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (_) => new MyAppTwo(
+                            routes: AppRoutes.DRIVER_DISPLAY,
+                          )));
             },
             child: Text(appConfig!.appDisplayName.toString())),
       ),
